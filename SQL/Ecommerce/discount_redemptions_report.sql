ec54c449c4c73619208bac88c8c1cfeea929cdec
caurodrig7 <caurodrig7@gmail.com>
2025-11-07 19:11:09 -0800

    Refactor Daily Discount Redemptions Report SQL script
    Refactor SQL script to improve readability and structure. Added CTEs for better organization and clarity.

--- SQL/Ecommerce/discount_redemptions_report.sql
+++ SQL/Ecommerce/discount_redemptions_report.sql
@@ -24,21 +24,71 @@ Author: Cesar Rodriguez
 --------------------------------------------------------------------------------
 */
 
-select sfcc.sfcc_discount.discount_type
-, sfcc.sfcc_discount.campaign_name
-, sfcc.sfcc_discount.promotion_name
-, sfcc.sfcc_discount.discount_name
-, count(sfcc.sfcc_discount.total) as total_redemptions
-, sum(sfcc.sfcc_discount.total) as total_discount
-from sfcc.sfcc_discount
-left join sfcc.sfcc_order
-  on sfcc.sfcc_order.sfcc_order_number = sfcc.sfcc_discount.sfcc_order_number
-where date(sfcc.sfcc_order.order_date) = CURRENT_DATE-1
-group by sfcc.sfcc_discount.discount_type
-, sfcc.sfcc_discount.campaign_name
-, sfcc.sfcc_discount.promotion_name
-, sfcc.sfcc_discount.discount_name
-order by sfcc.sfcc_discount.discount_type
-, sfcc.sfcc_discount.campaign_name
-, sfcc.sfcc_discount.promotion_name
-, sfcc.sfcc_discount.discount_name;
+WITH daily_discounts AS (
+    SELECT
+        d.discount_type,
+        d.campaign_name,
+        d.promotion_name,
+        d.discount_name,
+        d.total AS discount_amount,
+        o.order_date
+    FROM sfcc.sfcc_discount AS d
+    LEFT JOIN sfcc.sfcc_order AS o
+        ON o.sfcc_order_number = d.sfcc_order_number
+    WHERE CAST(o.order_date AS DATE) = CURRENT_DATE - INTERVAL '1' DAY
+),
+aggregated_discounts AS (
+    SELECT
+        discount_type,
+        campaign_name,
+        promotion_name,
+        discount_name,
+        COUNT(*) AS total_redemptions,
+        SUM(discount_amount) AS total_discount
+    FROM daily_discounts
+    GROUP BY
+        discount_type,
+        campaign_name,
+        promotion_name,
+        discount_name
+),
+ranked_discounts AS (
+    SELECT
+        ad.*,
+        SUM(ad.total_discount) OVER (
+            PARTITION BY ad.campaign_name
+        ) AS campaign_total_discount,
+        CASE 
+            WHEN SUM(ad.total_discount) OVER (PARTITION BY ad.campaign_name) = 0 
+                THEN 0
+            ELSE 
+                ROUND(
+                    100.0 * ad.total_discount 
+                    / SUM(ad.total_discount) OVER (PARTITION BY ad.campaign_name),
+                    2
+                )
+        END AS pct_of_campaign_discount,
+        RANK() OVER (
+            PARTITION BY ad.campaign_name
+            ORDER BY ad.total_discount DESC
+        ) AS promotion_rank_in_campaign
+    FROM aggregated_discounts AS ad
+)
+
+SELECT
+    discount_type,
+    campaign_name,
+    promotion_name,
+    discount_name,
+    total_redemptions,
+    total_discount,
+    campaign_total_discount,
+    pct_of_campaign_discount,
+    promotion_rank_in_campaign
+FROM ranked_discounts
+ORDER BY
+    discount_type,
+    campaign_name,
+    promotion_rank_in_campaign,
+    promotion_name,
+    discount_name;
